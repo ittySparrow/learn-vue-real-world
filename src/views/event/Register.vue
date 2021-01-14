2b4aa494b6598a6aa05f0fff7234a4f859bad6d7
ittySparrow <ittysparrow@gmail.com>
2021-01-14 13:02:26 +0300

    +vue route lessons (up to 6)

---
+++ src/views/event/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  methods: {
+    register() {
+      // There should be a call to API
+      this.$router.push({
+        name: 'EventDetails'
+        // params: { id: this.event.id } - do not need to do this
+      })
+    }
+  }
+}
+</script>
